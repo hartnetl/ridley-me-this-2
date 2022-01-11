986536b3e7ea90aaf68ad70ff3aa889d666af4a6
hartnetl <laura.hartnett@outlook.com>
2022-01-11 03:10:52 +0000

    Feat: Add total under basket icon

--- orders/models.py
+++ orders/models.py
@@ -72,6 +72,9 @@ class OrderItem(models.Model):
     def get_total_item_price(self):
         return self.quantity * self.item.price
 
+    def get_grand_total(self):
+        return self.get_total_item_price()
+
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -83,3 +86,10 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_grand_total()
+        return total
+
