9b18e4e6f43f1fdeab425d3f77342b1d0ef267b1
hartnetl <laura.hartnett@outlook.com>
2022-01-11 00:16:57 +0000

    Feat: Add functionality for toasts

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
 from django.utils import timezone
+from django.contrib import messages
 from orders.models import Product, OrderItem, Order
 
 
@@ -25,16 +26,19 @@ def add_to_basket(request, slug):
         # if it is, update the quantity
         if order.items.filter(item__slug=product.slug).exists():
             order_item.quantity += 1
+            messages.success(request, f'Updated quantity of {product.title} to {order_item.quantity}')
             order_item.save()
         # if it isn't, add the item to the order
         else:
             order.items.add(order_item)
+            messages.success(request, f'Added {product.title} to your basket')
     else:
     # if it doesn't exist we create a new order
         date_ordered = timezone.now()
         order = Order.objects.create(
             user=request.user, date_ordered=date_ordered)
         order.items.add(order_item)
+        messages.success(request, f'Added {product.title} to your new basket')
 
     return redirect("view_product", slug=slug)
 
@@ -51,6 +55,8 @@ def delete_product_from_basket(request, slug):
                 ordered=False
             )[0]
             order.items.remove(order_item)
+            messages.success(request, f'Removed {product.title} from your bag')
+
         else:
             # product doesn't exist in order
             return redirect("view_product", slug=slug)
