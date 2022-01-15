0dc9a965ab753633f07ec717b437f9f5bfe3fd5d
hartnetl <laura.hartnett@outlook.com>
2022-01-15 21:53:40 +0000

    Feat: Create new add_to_basket view and url

--- basket/urls.py
+++ basket/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
     # path('add/<slug>/', views.add_to_basket, name='add_to_basket'),
     # path('delete/<slug>/', views.delete_product_from_basket, name='remove_from_basket'),
     # path('remove/<slug>/', views.decrease_quantity, name='reduce_quantity'),
--- basket/views.py
+++ basket/views.py
@@ -11,3 +11,20 @@ def view_basket(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'basket/view_basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of the specified product to the shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
