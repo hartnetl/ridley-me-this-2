81abb07acf93db14bf820cafa72ed16017edb5d5
hartnetl <laura.hartnett@outlook.com>
2022-01-15 21:57:28 +0000

    Feat: Update contexts.py to create basket session

--- basket/contexts.py
+++ basket/contexts.py
@@ -3,15 +3,17 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from orders.models import Product
 
+
 def basket_contents(request):
     """
     This is a context processor.
-    Its purpose is to make this dictionary available to all templates across the entire application
+    Its purpose is to make this dictionary available to all templates across
+    the entire application
     """
 
-    # List for the bag items to live 
+    # List for the bag items to live
     basket_items = []
-    # starting total 
+    # starting total
     total = 0
     # empty basket
     product_count = 0
@@ -19,24 +21,17 @@ def basket_contents(request):
     basket = request.session.get('basket', {})
 
     # # For each item and quantity in bag
-    # for item_id, item_data in bag.items():
+    for item_id, item_data in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += item_data * product.price
+        product_count += item_data
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'product': product,
+        })
 
-    #     # execute this code if no size supplied and data is just an integer (for quantity)
-    #     if isinstance(item_data, int):
-    #         # Get the product
-    #         product = get_object_or_404(Product, pk=item_id)
-    #         # Add its quantity times the price to the total
-    #         total += item_data * product.price
-    #         # Increment the product count by the quantity
-    #         product_count += item_data
-    #         # add a dictionary to the list of bag items containing the id, quantity and the product object itself.
-    #         bag_items.append({
-    #             'item_id': item_id,
-    #             'quantity': item_data,
-    #             'product': product,
-    #         })
-
-    #     # execute this one if size is supplied 
+    #     # execute this one if size is supplied
     #     else:
     #         product = get_object_or_404(Product, pk=item_id)
     #         # iterate through inner dictionary of items by size
@@ -51,9 +46,9 @@ def basket_contents(request):
     #                 'size': size,
     #             })
 
-    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)    
+    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total
-    
+
     context = {
         'basket_items': basket_items,
         'total': total,
