746e614827d8475130a0f378bd4f838aacd2d120
hartnetl <laura.hartnett@outlook.com>
2022-01-16 01:34:28 +0000

    Feat: Set stripe public key and client secret in checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -11,7 +11,7 @@ from basket.contexts import basket_contents
 
 
 def checkout(request):
-
+    # stripe payment intent 
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -23,16 +23,25 @@ def checkout(request):
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    # set stripe secret keys 
     stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(amount=stripe_total,
                                          currency=settings.STRIPE_CURRENCY,)
+    print(intent)
 
+    # create empty instance of order form
     order_form = OrderForm()
+
+    # message incase you forget to set secret key 
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51K1HPjFEToCWPRVclerd629oZ2GPMA7MZ35nvCP1MFMF3TOaGag82Zcnss3Yks7VrpnTs54aTBofqbdW71E4mX19009CY8EerJ',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
