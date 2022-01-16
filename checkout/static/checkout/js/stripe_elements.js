a00ee9213ddd8f57181da65f472e9f2d4d5c342c
hartnetl <laura.hartnett@outlook.com>
2022-01-16 01:41:15 +0000

    Feat: Add js to handle checkout form submission to stripe

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -74,87 +74,28 @@ card.addEventListener('change', function (event) {
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
-    // prevent the default action, which here is Post 
     ev.preventDefault();
-    // disable card element and submit button to prevent multiple submissions 
-    card.update({'disabled': true});
-    // overlay loading 
+    card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
-    $('#payment-form').fadeToggle(100);
-    $('#loading-overlay').fadeToggle(100);
-
-    // get boolean of save info by checking it's checked attribute 
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    // From using {% csrf_token %} in the form
-    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-    // create a small object to pass this information to the new view and also
-    // pass the client secret for the payment intent
-    var postData = {
-        'csrfmiddlewaretoken': csrfToken,
-        'client_secret': clientSecret,
-        'save_info': saveInfo,
-    };
-    // create variable for new url 
-    var url = '/checkout/cache_checkout_data/';
-
-    // post data to view and when that's done call the confirmcardpayment function
-    $.post(url, postData).done(function () {
-        // Call confirm card payment method 
-        stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: card,
-                billing_details: {
-                    name: $.trim(form.full_name.value),
-                    phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
-                    address: {
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
-                    }
-                }
-            },
-            shipping: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
-                address: {
-                    line1: $.trim(form.street_address1.value),
-                    line2: $.trim(form.street_address2.value),
-                    city: $.trim(form.town_or_city.value),
-                    country: $.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
-                    state: $.trim(form.county.value),
-                }
-            },
-            // execute this function on the result of confirmCardPayment 
-        }).then(function(result) {
-            // If there's an error put it into the card error div 
-            if (result.error) {
-                var errorDiv = document.getElementById('card-errors');
-                var html = `
-                    <span class="icon" role="alert">
-                    <i class="fas fa-times"></i>
-                    </span>
-                    <span>${result.error.message}</span>`;
-                $(errorDiv).html(html);
-                // overlay loading 
-                $('#payment-form').fadeToggle(100);
-                $('#loading-overlay').fadeToggle(100);
-                // Enable card element and submit button to allow users to fix error 
-                card.update({'disabled': false});
-                $('#submit-button').attr('disabled', false);
-            } else {
-                // If the status of payment intent comes back as succeeded, submit the fprm 
-                if (result.paymentIntent.status === 'succeeded') {
-                    form.submit();
-                }
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
             }
-        });
-    // Error if view sens 400 error 
-    }).fail(function () {
-        // just reload the page, the error will be in django messages
-        location.reload();
-    })
+        }
+    });
 });
