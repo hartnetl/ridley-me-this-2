02a8b8f5fbe242fa7f24b23fa8d85e62c8cf1630
hartnetl <laura.hartnett@outlook.com>
2022-01-16 15:21:56 +0000

    Feat: Update handlepaymentintentsucceeded to handle orders that do or don't exist and include a delay for slow servers

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -65,7 +65,7 @@ class StripeWH_Handler:
                 time.sleep(1)
 
         if order_exists:
-            self._send_confirmation_email(order)
+            # self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
