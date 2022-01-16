fb40c9c57ce60d70a77c7424cc636801309fcedf
hartnetl <laura.hartnett@outlook.com>
2022-01-16 15:31:21 +0000

    Add pid and original basket to views to ensure correct order is found

--- checkout/views.py
+++ checkout/views.py
@@ -57,7 +57,11 @@ def checkout(request):
         order_form = OrderForm(form_data)
         # If form is valid, save the order
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(basket)
+            order.save()
             # iterate through each basket item to create each line item
             for item_id, item_data in basket.items():
                 try:
