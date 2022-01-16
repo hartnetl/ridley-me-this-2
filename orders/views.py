e8202ff7984c0aaa3c5efad102aaf337355be438
hartnetl <laura.hartnett@outlook.com>
2022-01-16 21:03:31 +0000

    Feat: Create add_product view

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
+from .forms import ProductForm
 
 
 class productsView(ListView):
@@ -11,3 +12,14 @@ class productsView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = "orders/product_detail.html"
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
