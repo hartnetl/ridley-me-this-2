ddc6d6ba258189387c5d9de3704e92d48f826760
hartnetl <laura.hartnett@outlook.com>
2022-01-16 23:48:37 +0000

    SAVE: about to deploy - making a copy of this repo just in case it goes wrong

--- main_app/settings.py
+++ main_app/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import stripe
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*bvb1urf!#-f&ajdx)tf1sq#=t_7%h2qel-b_x2bx*lxh(66gw'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 stripe.api_key = 'sk_test_51K1HPjFEToCWPRVcmmrRxsCjJKuU0l7R9t0v452RamNdTcNkVQtWBSV7PXSSCFQJxCP26LSZ1ThBMfxvBqBdzcxb00sCw4NGpr'
 
@@ -125,12 +130,15 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+
 
 
 # Password validation
