4d4a7c21c8447d4c0c95531f7cfc295ca20ad80f
hartnetl <laura.hartnett@outlook.com>
2022-01-17 12:55:20 +0000

    Feat: Add cache settings

--- main_app/settings.py
+++ main_app/settings.py
@@ -186,6 +186,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control 
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'ridley-me-this-app'
     AWS_S3_REGION_NAME = 'eu-west-1'
