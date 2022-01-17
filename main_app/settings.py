effddce004d72e9e59c57aef6262986251caebe6
hartnetl <laura.hartnett@outlook.com>
2022-01-17 14:43:20 +0000

    feat: Add image preview to edit product form

--- main_app/settings.py
+++ main_app/settings.py
@@ -191,7 +191,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'ridley-me-this-app'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -199,15 +199,15 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# Static and media files
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-STATICFILES_LOCATION = 'static'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-MEDIAFILES_LOCATION = 'media'
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
 
-# Override static and media urls in production
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    # Override static and media urls in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 # Default primary key field type
---
+++ orders/widgets.py
@@ -0,0 +1,12 @@
+# import this file https://github.com/django/django/blob/main/django/forms/widgets.py
+from django.forms.widgets import ClearableFileInput
+ # this is userd for translation
+from django.utils.translation import gettext_lazy as _
+
+# create a custom class which inherits from the original one
+class CustomClearableFileInput(ClearableFileInput):
+    # override these settings
+    clear_checkbox_label = _('Remove')
+    initial_text = _('Current Image')
+    input_text = _('')
+    template_name = 'orders/custom_widget_templates/custom_clearable_file_input.html'
