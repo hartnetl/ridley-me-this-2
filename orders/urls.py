e09bc69cad520c11ce7e567a8c19d524fd3d149f
hartnetl <laura.hartnett@outlook.com>
2022-01-19 14:57:19 +0000

    Fix: Get editProduct form working
    view was fine, it was called wrong in the html

--- orders/urls.py
+++ orders/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     # path('add/', views.AddProduct.as_view(), name='add_product'),
     path('add/', views.AddProduct, name='add_product'),
     path('<slug>/', views.ProductDetailView.as_view(), name='view_product'),
-    path('edit/<slug>/', views.EditProduct.as_view(), name='edit_product'),
+    path('edit/<slug>/', views.EditProduct, name='edit_product'),
+    # path('edit/<slug>/', views.EditProduct.as_view(), name='edit_product'),
     path('delete/<slug>/', views.delete_product, name='delete_product'),
     # path('<int:product_id>/', views.ProductDetailView.as_view(), name='view_product'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -57,12 +57,90 @@ def AddProduct(request):
                 return redirect(reverse('view_product', args=[product.slug]))
             else:
                 turtle_details_formset = TurtleFormset(request.POST)
+        else:
+            messages.error(request, 'Failed to add product, please ensure form is filled correctly')
+    else:
+        product_form = ProductForm()
 
     context = {
         'product_form': product_form,
         'turtle_details_form': turtle_details_formset
     }
     return render(request, 'orders/add_product.html', context=context)
+
+
+@login_required
+def EditProduct(request, slug):
+    # user must be superuser
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, slug=slug)
+    turtle_details_formset = TurtleFormset(product)
+
+    if request.method == 'POST':
+        print('IS THIS THE PROBLEM')
+        product_form = ProductForm(request.POST, instance=product)
+
+        if product_form.is_valid():
+            print("PRODUCT FORM IS VALID")
+            products = product_form.save(commit=False)
+            turtle_details_formset = TurtleFormset(request.POST, instance=products)
+
+            if turtle_details_formset.is_valid():
+                print("TURTLE DETAILS FORM IS VALID")
+                product_form.save()
+                turtle_details_formset.save()
+                messages.success(request, f"Successfully updated '{product.title}'")
+                # return redirect('products')
+                return redirect(reverse('view_product', args=[product.slug]))
+            else:
+                print("TURTLE FORM NOT VALID")
+                messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+                turtle_details_formset = TurtleFormset(request.POST)
+        else:
+            messages.error(request, 'Failed to add product, please ensure form is filled correctly')
+    else:
+        print("PRODUCT FORM NOT VALID")
+        product_form = ProductForm(instance=product)
+        turtle_details_formset = TurtleFormset(instance=product)
+
+    context = {
+        'product_form': product_form,
+        'turtle_details_form': turtle_details_formset,
+        'product': product
+    }
+    return render(request, 'orders/edit_products.html', context=context)
+
+
+# class AddProduct(CreateView):
+#     model = Product
+#     form_class = ProductForm
+#     template_name = 'orders/add_product.html'
+#     success_message = "Successfully added '%(title)s'"
+
+#     def get_success_url(self):
+#         return reverse('view_product', kwargs={'slug': self.object.slug})
+
+
+#     def get_context_data(self, **kwargs):
+#         context = super(AddProduct, self).get_context_data(**kwargs)
+
+#         if self.request.POST:
+#             context['turtle_formset'] = TurtleFormset(self.request.POST)
+#         else:
+#             context['turtle_formset'] = TurtleFormset()
+#         return context
+
+#     def form_valid(self, form):
+#         context = self.get_context_data()
+#         turtle_details_form = context['TurtleFormset']
+#         if turtle_details_form.is_valid():
+#             self.object = form.save()
+#             turtle_details_form.instance = self.object
+#             turtle_details_form.save()
+#             return redirect(reverse('view_product', args=[product.slug]))
 
 
 # class AddProduct(SuperUserRequiredMixin, SuccessMessageMixin, CreateView):
@@ -80,15 +158,16 @@ def AddProduct(request):
 #         return super().form_valid(form)
 
 
-class EditProduct(SuperUserRequiredMixin, SuccessMessageMixin, UpdateView):
-    model = Product
-    form_class = ProductForm
-    template_name = 'orders/edit_products.html'
-    success_message = "Successfully updated '%(title)s'"
+# class EditProduct(SuperUserRequiredMixin, SuccessMessageMixin, UpdateView):
+#     model = Product
+#     form_class = ProductForm
+#     template_name = 'orders/edit_products.html'
+#     success_message = "Successfully updated '%(title)s'"
 
-    def get_success_url(self):
-        return reverse('view_product', kwargs={'slug': self.object.slug})
+#     def get_success_url(self):
+#         return reverse('view_product', kwargs={'slug': self.object.slug})
 
+@login_required
 
 @login_required
 def delete_product(request, slug):
