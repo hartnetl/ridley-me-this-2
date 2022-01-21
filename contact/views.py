980c9bc2ba1c6a98b13350af6c32ba11b3c904c5
hartnetl <laura.hartnett@outlook.com>
2022-01-21 16:22:53 +0000

    Feat: Create contact form.py and add to template

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
 def contact_page(request):
-    return render(request, 'contact/contact.html')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
