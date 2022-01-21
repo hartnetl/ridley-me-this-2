5e84dedb4e8213260a195938e452ae33df5e3234
hartnetl <laura.hartnett@outlook.com>
2022-01-21 13:24:19 +0000

    Feat: Add category filtering for products page

--- main_app/settings.py
+++ main_app/settings.py
@@ -227,7 +227,7 @@ DEFAULT_FROM_EMAIL = 'ridleymethis@example.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'ridleymethis@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
