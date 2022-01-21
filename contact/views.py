4b9ea086da16e6bcb6c02bf0b7ba7a07e2350507
hartnetl <laura.hartnett@outlook.com>
2022-01-21 16:26:21 +0000

    Feat: Add success message when contact form is submitted

--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,11 @@ from .forms import ContactForm
 
 # Create your views here.
 def contact_page(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'contact/contact_success.html')
     form = ContactForm()
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
