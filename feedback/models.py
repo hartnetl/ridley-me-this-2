a5e54d86078d93b4984c7b138686701f270c2e6c
hartnetl <laura.hartnett@outlook.com>
2022-01-22 13:47:41 +0000

    Feat: Create feedback app with testimonials and admin_comment models

---
+++ feedback/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Testimonials(models.Model):
+    reviewed_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                    related_name="reviews")
+    reviewed_on = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(max_length=10, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    content = models.TextField(max_length=500)
+    icon = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['reviewed_on']
+
+    def __str__(self):
+        return f" {self.rating} by {self.User.name}"
+
+
+class admin_comments(models.Model):
+    review = models.ForeignKey(Testimonials, on_delete=models.SET_NULL, related_name='comments')
+    name = models.CharField(max_length=50)
+    date_replied = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.comment} replied by {self.name}"
