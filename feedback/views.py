d8a251cb1df84bc91a0cdbfd6328337bec00e965
hartnetl <laura.hartnett@outlook.com>
2022-01-22 13:52:49 +0000

    Feat: Create testimonal view

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Testimonials
 
-# Create your views here.
+
+class TestimonialView(generic,ListView):
+    model = Testimonials
+    template_name = 'feedback/testimonials.html'
+    paginate_by = 6
