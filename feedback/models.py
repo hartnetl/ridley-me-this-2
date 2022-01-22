d3c418cb47d02eddac1589d1590fcfea7c1835ca
hartnetl <laura.hartnett@outlook.com>
2022-01-22 14:19:54 +0000

    Feat: Render testimonials on template (not finished)

--- feedback/models.py
+++ feedback/models.py
@@ -7,7 +7,7 @@ class Testimonials(models.Model):
     reviewed_by = models.ForeignKey(User, on_delete=models.CASCADE,
                                     related_name="reviews")
     reviewed_on = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(max_length=10, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     content = models.TextField(max_length=500)
     icon = models.ImageField(null=True, blank=True)
 
@@ -15,11 +15,11 @@ class Testimonials(models.Model):
         ordering = ['reviewed_on']
 
     def __str__(self):
-        return f" {self.rating} by {self.User.name}"
+        return f"{self.reviewed_by.username} gave a rating of {self.rating}"
 
 
 class admin_comments(models.Model):
-    review = models.ForeignKey(Testimonials, on_delete=models.SET_NULL, related_name='comments')
+    review = models.ForeignKey(Testimonials, on_delete=models.SET_NULL, related_name='comments', null=True)
     name = models.CharField(max_length=50)
     date_replied = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
